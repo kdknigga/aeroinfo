bc3b730f167cc09f4f6f5bb35f16c90a3bef6402
Kris Knigga <kris@knigga.com>
2022-12-02 15:46:18 -0600

    CLEAR beacon color changed to WHITE

---
+++ alembic/versions/202212021923_618d19385afc_clear_beacon_color_changed_to_white.py
@@ -0,0 +1,38 @@
+"""CLEAR beacon color changed to WHITE
+
+Revision ID: 618d19385afc
+Revises: 0bf3e62ca4e2
+Create Date: 2022-12-02 19:23:34.125444+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "618d19385afc"
+down_revision = "0bf3e62ca4e2"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_bind()
+    if bind.engine.name == "postgresql":
+        with op.get_context().autocommit_block():
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'CG' TO 'WG'")
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'CY' TO 'WY'")
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'CGY' TO 'WGY'")
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'SCG' TO 'SWG'")
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'C' TO 'W'")
+
+
+def downgrade():
+    bind = op.get_bind()
+    if bind.engine.name == "postgresql":
+        with op.get_context().autocommit_block():
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'WG' TO 'CG'")
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'WY' TO 'CY'")
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'WGY' TO 'CGY'")
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'SWG' TO 'SCG'")
+            op.execute("ALTER TYPE beaconcolorenum RENAME VALUE 'W' TO 'C'")
